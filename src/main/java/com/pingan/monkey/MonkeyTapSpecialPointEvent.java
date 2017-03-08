9af8f52177581b1b0b6c996245c2110a4d477a65
baozhida <baozhida88@126.com>
2017-03-08 13:24:41 +0800

    Add files via upload

---
+++ src/main/java/com/pingan/monkey/MonkeyTapSpecialPointEvent.java
@@ -0,0 +1,31 @@
+package com.pingan.monkey;
+
+import com.alibaba.fastjson.JSONObject;
+import macaca.client.MacacaClient;
+
+/**
+ * Created by bzd on 2017.3.8 途牛app特殊的需要
+ */
+public class MonkeyTapSpecialPointEvent extends MonkeyEvent{
+    private int width, height;
+    private MacacaClient driver;
+
+
+    public MonkeyTapSpecialPointEvent(MacacaClient driver, int width, int height) {
+        super(MonkeyEvent.EVENT_TYPE_TAP);
+        this.width = width;
+        this.height = height;
+        this.driver = driver;
+
+    }
+
+
+    public int injectEvent() throws Exception {
+        System.out.println("sending SpecialPoint Tap Event : Tap->(" + this.width + ", " + this.height + ")");
+        JSONObject jSONObject = new JSONObject();
+        jSONObject.put("x", this.width);
+        jSONObject.put("y", this.height);
+        driver.touch("tap", jSONObject);
+        return MonkeyEvent.INJECT_SUCCESS;
+    }
+}
